a555abf5de11f2e5c1c0c35e8b5e2e4ff3260e7d
MarcusVinicius1711 <mvbs.1711@gmail.com>
2025-11-26 16:47:41 -0300

    Teste5

--- models/db.js
+++ models/db.js
@@ -6,12 +6,12 @@ dotenv.config({ path: "./.env" });
 
 // Exemplo para MySQL
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "postgres",
     dialectModule: postgres,
     logging: false,
